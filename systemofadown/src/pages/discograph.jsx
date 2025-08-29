fe0f5089c517da4689e5ffb62c70f050681ea146
CauaOliveB <Cauaolivebarbosa32250458@gmail.com>
2025-08-29 11:32:01 -0300

    Semi-concluded git commit -m Semi-concluded
    git commit -m Semi-concluded

--- systemofadown/src/pages/discograph.jsx
+++ systemofadown/src/pages/discograph.jsx
@@ -1,6 +1,58 @@
-const Discograph = () => {
-    // (
+import Header from '../components/header';
+import Footer from '../components/footer';
+import styles from '../styles/discograph.module.css';
+import soadLiveImage from '../assets/soadLive.png';
+import chopSueyVideo from '../assets/chopsuey.png';
+import aerialsVideo from '../assets/aerials.png';
+import lonelyDayVideo from '../assets/lonelyDay.png';
 
-    // );
+const Discograph = () => {
+ return (
+    <>
+      <Header />
+      <div className={styles.main}>
+        <div className={styles.musicHeader}>
+          <h1>SYSTEM OF A DOWN</h1>
+          <img src={soadLiveImage} alt="System of a Down Live Concert" className={styles.soadLive} />
+          <div className={styles.quoteBlock}>
+            <h2>Music</h2>
+            <p>
+              "SOAD's music is a mix of metal, punk, Armenian folk, and experimental
+              sounds. Their lyrics cover war, politics, inequality, and human
+              struggles, always with intensity and emotion."
+            </p>
+          </div>
+        </div>
+
+        <section className={styles.videoSection}>
+          <div className={styles.videoItem}>
+            <iframe width="460" height="218" src="https://www.youtube.com/embed/CSvFpBOe8eY?si=APlPM7kYqPZMxwM6" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
+            referrerpolicy="strict-origin-when-cross-origin" allowfullscreen frameborder="0"></iframe>
+            <div className={styles.videoInfo}>
+              <h3>System Of A Down - Chop Suey! (Official HD Video)</h3>
+              <p>1.4 Bi Views • At 15 years ago</p>
+            </div>
+          </div>
+          <div className={styles.videoItem}>
+            <iframe width="420" height="218" src="https://www.youtube.com/embed/L-iepu3EtyE?si=wM7EabO8bx6YpgpJ" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
+            referrerpolicy="strict-origin-when-cross-origin" allowfullscreen frameborder="0"></iframe>
+            <div className={styles.videoInfo}>
+              <h3>System Of A Down - Aerials (Official HD Video)</h3>
+              <p>517 Mi Views • At 15 years ago</p>
+            </div>
+          </div>
+          <div className={styles.videoItem}>
+            <iframe width="460" height="218" src="https://www.youtube.com/embed/DnGdoEa1tPg?si=qnW96lgQ7EZCBwPr" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
+            referrerpolicy="strict-origin-when-cross-origin" allowfullscreen frameborder="0"></iframe>
+            <div className={styles.videoInfo}>
+              <h3>System Of A Down - Lonely Day (Official HD Video)</h3>
+              <p>423 Mi Views • At 15 years ago</p>
+            </div>
+          </div>
+        </section>
+      </div>
+      <Footer />
+    </>
+  );
 };
 export default Discograph;
--- systemofadown/src/pages/historic.jsx
+++ systemofadown/src/pages/historic.jsx
@@ -1,6 +1,36 @@
-const Historic = () => {
-    // (
+import { Link } from "react-router-dom";
+import Header from "../components/header";
+import Footer from "../components/footer";
+import styles from "../styles/historic.module.css";
+import Background from "../assets/historicBackground.png"
+import SOAD from "../assets/SOAD.png"
 
-    // );
+
+const Historic = () => {
+return (
+    <>
+    <Header />
+    <img src={SOAD} alt="SOAD Group Image" className={styles.SOADGroup} />
+    <img src={Background} className={styles.background}/>
+    
+    <main>
+    <section className={styles.informationBlock}>
+        <h2>History & Social Struggle</h2>
+        <p>
+            System of a Down was formed in Los Angeles between 1994 and 1998 by four Armenian-descendant musicians who immediately used their platform to raise awareness about the Armenian Genocide. After gaining attention in underground clubs, they signed with producer Rick Rubin and released their debut album in 1998, introducing an aggressive and experimental new sound with songs like Sugar and Spiders.
+            Their breakthrough came with Toxicity (2001), which propelled them to worldwide fame thanks to anthems like Chop Suey!, Toxicity, and Aerials. Released just before 9/11, some of their politically charged songs faced censorship, reinforcing their image as a band unafraid to challenge authority.
+            Between 2004 and 2006, System of a Down became deeply involved in activism, opposing the Iraq War, demanding recognition of the Armenian Genocide, and supporting humanitarian causes in Armenia. Despite going on hiatus in 2006 due to internal conflicts, they reunited in 2010 and have since maintained cult status. In 2020, they released new songs, Protect the Land and Genocidal Humanoidz, in support of Armenia during the Nagorno-Karabakh conflict.
+        </p>
+        <blockquote className={styles.quote}>
+            “System of a Down was never just about music. They became a voice against war, inequality, and historical denial, using their art as a form of resistance.”
+        </blockquote>
+    </section>
+    </main>
+
+
+    <Footer/>
+    </>
+    
+);
 };
 export default Historic;
